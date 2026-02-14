3c55973f22b749aca17da1f3a0e8c7671945c39e
EngineX-GB <uk.engine.x@gmail.com>
2026-02-14 11:57:17 +0000

    Created an adhoc script for copying media data

---
+++ src/adhoc/ImageFileHelper.py
@@ -0,0 +1,67 @@
+import os
+import shutil
+import sys
+
+
+# Script: ImageFileHelper.py
+# An adhoc script to copy specific user files to a temporary location that can be extracted later.
+# 2026-02-14: This is yet to be tested properly before use.
+
+class ImageFileHelper:
+
+    def __init__(self):
+        pass
+
+    def copy_image_data(self, user_id_set, src_image_directory, dest_image_directory):
+        if not os.path.exists(src_image_directory):
+            print(f"[ERROR] {src_image_directory} does not exist")
+        if not os.path.exists(dest_image_directory):
+            os.makedirs(dest_image_directory, exist_ok=True)
+        for user_id in user_id_set:
+            source_user_folder = f"{src_image_directory}/{user_id}"
+            destination_user_folder = f"{dest_image_directory}/{user_id}"
+            if not os.path.exists(source_user_folder):
+                print(f"[ERROR] Directory {source_user_folder} does not exist")
+            else:
+                # copy the directory and it's contents into the new folder
+                print(f"[INFO] Copying {source_user_folder} -> {destination_user_folder} ")
+                shutil.copytree(source_user_folder, destination_user_folder, dirs_exist_ok=True)
+
+    def delete_image_data(self, user_id_set, image_directory):
+        if not os.path.exists(image_directory):
+            print(f"[ERROR] {image_directory} does not exist")
+        for user_id in user_id_set:
+            user_folder = f"{image_directory}/{user_id}"
+            if not os.path.exists(user_folder):
+                print(f"[ERROR] Directory {user_folder} does not exist")
+            else:
+                # copy the directory and it's contents into the new folder
+                print(f"[INFO] Deleting {user_folder}")
+                shutil.rmtree(user_folder)
+
+    def read_user_id_list(self, filepath: str):
+        if not os.path.exists(filepath):
+            print(f"[ERROR] {filepath} does not exist")
+            return set()
+        else:
+            f = open(filepath, 'r', encoding='utf-8')
+            user_ids = f.readlines()
+            f.close()
+            return set(user_ids)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        image_file_helper = ImageFileHelper()
+        user_id_set = image_file_helper.read_user_id_list("C:/Users/user-id-list.txt")
+        if len(user_id_set):
+            print("[WARN] List is empty. Exiting")
+            exit(0)
+        if sys.argv[1] == "copy":
+            image_file_helper.copy_image_data(user_id_set, "C:/Users/img/src", "C:/Users/img/dest")
+            print("[INFO] Copying completed.")
+        if sys.argv[1] == "delete":
+            image_file_helper.delete_image_data(user_id_set, "C:/Users/img/src")
+            print("[INFO] Deletion completed.")
+    else:
+        print("[ERROR] Script must have at least one argument.")
