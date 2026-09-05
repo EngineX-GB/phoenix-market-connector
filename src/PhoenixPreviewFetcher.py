d7f4e2b44fa6682f86958c2e85fd4fd3e0dc7198
EngineX-GB <uk.engine.x@gmail.com>
2026-09-05 18:51:32 +0100

    Adding previews loader (2)

--- src/PhoenixPreviewFetcher.py
+++ src/PhoenixPreviewFetcher.py
@@ -1,10 +1,11 @@
 import json
 import time
+from datetime import datetime, timedelta
 
 import requests
 
 from PhoenixAbstractV2DataLoader import PhoenixAbstractV2DataLoader
-from model.Preview import Preview, PreviewLine, PreviewDataHolder
+from model.Preview import Preview, PreviewCollapsedLine, PreviewDataHolder, PreviewExpandedLine
 
 
 class PhoenixPreviewFetcher(PhoenixAbstractV2DataLoader):
@@ -15,19 +16,21 @@ class PhoenixPreviewFetcher(PhoenixAbstractV2DataLoader):
 
     def load(self, data):
         preview_headers = []
-        preview_lines = []
+        preview_collapsed_lines = []
+        preview_expanded_lines = []
         results = data["Result"]
         for result in results:
             user_id = result["userId"]
             title = str(result["title"])
             details = str(result["details"])
+            #datetime.strptime(start_date, "%Y-%m-%dT%H:%M:%S").strftime("%Y-%m-%d")
             start_date = result["startDate"]
             end_date = result["endDate"]
             is_active = str(result["isActive"])
             business_key = str(user_id) + "_" + start_date + "_" + end_date + "_" + str(is_active)
 
             preview_headers.append(Preview(str(user_id), title, details, start_date,
-                                                end_date, str(is_active), business_key).generate_record())
+                                           end_date, str(is_active), business_key))
 
             # get the number of stops on the preview
             stops = result["stops"]
@@ -40,11 +43,33 @@ class PhoenixPreviewFetcher(PhoenixAbstractV2DataLoader):
                 details = str(stop["details"])
                 start_date = stop["startDate"]
                 end_date = stop["endDate"]
+                #
+                # start_date_converted = datetime.strptime(start_date, "%Y-%m-%dT%H:%M:%S").strftime("%Y-%m-%d")
+                # end_date_converted = datetime.strptime(end_date, "%Y-%m-%dT%H:%M:%S").strftime("%Y-%m-%d")
 
-                preview_lines.append(PreviewLine(str(user_id), country, region, county, town, postcode,
-                                                      details, start_date, end_date, business_key).generate_record())
+                preview_collapsed_lines.append(PreviewCollapsedLine(str(user_id), country, region, county, town,
+                                                                    postcode, details, start_date, end_date,
+                                                                    business_key))
 
-        return PreviewDataHolder(preview_headers, preview_lines)
+            # now take the "collapsed" preview lines (with start date and end date) and break up the line to
+            # individual dates, where each line is a date of which the user is available within the start and end
+            # date range.
+
+            for line in preview_collapsed_lines:
+                _start_date = datetime.strptime(line.start_date, "%Y-%m-%dT%H:%M:%S").date()
+                _end_date = datetime.strptime(line.end_date, "%Y-%m-%dT%H:%M:%S").date()
+
+                _dates = []
+
+                while _start_date <= _end_date:
+                    preview_expanded_lines.append(
+                        PreviewExpandedLine(line.user_id, line.country, line.region, line.county,
+                                            line.town, line.postcode, line.details,
+                                            _start_date.strftime("%Y-%m-%d"),
+                                            line.business_key))
+                    _start_date += timedelta(days=1)
+
+        return PreviewDataHolder(preview_headers, preview_collapsed_lines, preview_expanded_lines)
 
     # This function is for reading off a user id list for the day
     def execute(self):
@@ -60,22 +85,24 @@ class PhoenixPreviewFetcher(PhoenixAbstractV2DataLoader):
                     headers = {"Content-Type": "application/json",
                                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"}
 
-
-                    response = requests.get(f"{self.propertyManager.getApiProviderUrl()}/api/Content/Tour/{user_id}?v=2", headers=headers)
+                    response = requests.get(
+                        f"{self.propertyManager.getApiProviderUrl()}/api/Content/Tour/{user_id}?v=2", headers=headers)
 
                     # add a pause here to control the rate of API invocation
                     time.sleep(1)
                     if response.status_code != 200:
-                        print(f"An error has occurred when fetching preview data for {user_id}. Status code: {response.status_code}")
+                        print(
+                            f"An error has occurred when fetching preview data for {user_id}. Status code: {response.status_code}")
                     else:
                         preview_data_holder = self.load(response.json())
                         if len(preview_data_holder.get_preview_header()) > 0:
                             # here, you can either put the data in a csv file, or if it's in service mode
                             # then send it to the ingestion service to store in the database
 
+                            # here, we will just get the expanded rows (individual days)
                             print("===================")
-                            for p in preview_data_holder.get_preview_header():
-                                print(p)
+                            for p in preview_data_holder.get_preview_expanded_lines():
+                                print(p.generate_record())
                             print("===================")
 
                             # if successful, then update the temp list for preview files, so the same user data is not
--- src/model/Preview.py
+++ src/model/Preview.py
@@ -31,8 +31,7 @@ class Preview:
         return self.user_id + "|" + self.title + "|" + self.details + "|" + self.start_date + "|" + self.end_date + "|" + self.business_key
 
 
-
-class PreviewLine:
+class PreviewCollapsedLine:
 
     def __init__(self, user_id, country, region, county, town, postcode, details, start_date, end_date, business_key):
         self.user_id = user_id
@@ -79,14 +78,63 @@ class PreviewLine:
     def get_business_key(self):
         return self.business_key
 
+
+class PreviewExpandedLine:
+
+    def __init__(self, user_id, country, region, county, town, postcode, details, availability_date, business_key):
+        self.user_id = user_id
+        self.country = country
+        self.region = region
+        self.county = county
+        self.town = town
+        self.postcode = postcode
+        self.details = details
+        self.availability_date = availability_date
+        self.business_key = business_key
+
+    def generate_record(self):
+        return self.user_id + "|" + self.country + "|" + self.region + "|" + self.county + "|" + self.town + "|" + self.postcode + "|" + self.details + "|" + self.availability_date + "|" + self.business_key
+
+    def get_user_id(self):
+        return self.user_id
+
+    def get_country(self):
+        return self.country
+
+    def get_region(self):
+        return self.region
+
+    def get_county(self):
+        return self.county
+
+    def get_town(self):
+        return self.town
+
+    def get_postcode(self):
+        return self.postcode
+
+    def get_details(self):
+        return self.details
+
+    def get_availability_date(self):
+        return self.availability_date
+
+    def get_business_key(self):
+        return self.business_key
+
+
 class PreviewDataHolder:
 
-    def __init__(self, preview_headers, preview_lines):
+    def __init__(self, preview_headers, preview_collapsed_lines, preview_expanded_lines):
         self.preview_headers = preview_headers
-        self.preview_lines = preview_lines
+        self.preview_collapsed_lines = preview_collapsed_lines
+        self.preview_expanded_lines = preview_expanded_lines
 
     def get_preview_header(self):
         return self.preview_headers
 
-    def get_preview_lines(self):
-        return self.preview_lines
+    def get_preview_collapsed_lines(self):
+        return self.preview_collapsed_lines
+
+    def get_preview_expanded_lines(self):
+        return self.preview_expanded_lines
